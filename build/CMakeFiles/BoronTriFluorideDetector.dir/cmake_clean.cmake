a84796cb577274cad6d3fc0f8b28db5d98f59fb2
codeyolson1 <codey.olson@utah.edu>
2022-02-23 08:18:54 -0700

    Seperated BoronTriFluroideDetector to stand-alone application.

---
+++ build/CMakeFiles/BoronTriFluorideDetector.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BoronTriFluorideDetector"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/BoronTriFluorideDetector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/bf3.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bf3.dir/main.cc.o"
+  "CMakeFiles/bf3.dir/src/ActionInitialization.cc.o"
+  "CMakeFiles/bf3.dir/src/Analysis.cc.o"
+  "CMakeFiles/bf3.dir/src/DetectorConstruction.cc.o"
+  "CMakeFiles/bf3.dir/src/EventAction.cc.o"
+  "CMakeFiles/bf3.dir/src/PhysicsList.cc.o"
+  "CMakeFiles/bf3.dir/src/PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/bf3.dir/src/Run.cc.o"
+  "CMakeFiles/bf3.dir/src/RunAction.cc.o"
+  "bf3"
+  "bf3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bf3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
